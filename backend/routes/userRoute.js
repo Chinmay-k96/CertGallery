b48facdd47fc96c1848886013f9812949f1a4ad0
chinmay kulkarni <chinmaysk1996@gmail.com>
2024-12-06 17:48:06 +0530

    added cookie options

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,7 +9,11 @@ router.post("/login", (req, res) => {
   console.log("USER_PIN", process.env.USER_PIN)
   if (Number(mpin) === Number(process.env.USER_PIN)) {
     const token = generateToken(mpin);
-    res.cookie("usertoken", token);
+    res.cookie("usertoken", token, {
+      httpOnly: false,     // Makes the cookie inaccessible via JavaScript
+      secure: true,       // Ensures the cookie is sent over HTTPS
+      sameSite: "None",   // Required for cross-origin cookies
+    });
     res.send("Login successfull");
   } else {
     res.status(401);
