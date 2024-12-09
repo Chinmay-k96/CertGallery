425ff80f0ab5d35b14c419acdef2df2cc3b4fcce
chinmay kulkarni <chinmaysk1996@gmail.com>
2024-12-09 14:23:40 +0530

    domain and path

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -10,10 +10,12 @@ router.post("/login", (req, res) => {
   if (Number(mpin) === Number(process.env.USER_PIN)) {
     const token = generateToken(mpin);
     res.cookie("usertoken", token, {
-      httpOnly: false,     // Makes the cookie inaccessible via JavaScript
+      httpOnly: true,     // Makes the cookie inaccessible via JavaScript
       secure: true,       // Ensures the cookie is sent over HTTPS
       sameSite: "None",   // Required for cross-origin cookies
-      maxAge: 1000 * 60 * 60 * 24 * 2
+      maxAge: 1000 * 60 * 60 * 24 * 2,
+      path: "/api",
+      domain: process.env.ALLOWED_ORIGIN
     });
     res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN);
     res.header('Access-Control-Allow-Credentials', 'true');
