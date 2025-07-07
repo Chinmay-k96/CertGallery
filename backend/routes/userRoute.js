338eec4b7be06d2f122fb62368b9902fab2ac81e
chinmay kulkarni <chinmaysk1996@gmail.com>
2025-07-08 00:24:40 +0530

    single origin for access-control-allow-origin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,10 +5,16 @@ import generateToken from "../utils/generateToken.js";
 //login  - public
 router.post("/login", (req, res) => {
   const { mpin } = req.body;
+  const origin = req.headers.origin;
+  const allowedOrigins = process.env.ALLOWED_ORIGIN?.split(",") || [];
+
+  // If the request's Origin is in your allowlist, echo it back
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+  }
   if (Number(mpin) === Number(process.env.USER_PIN)) {
     const token = generateToken();
-    res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN);
-    res.header('Access-Control-Allow-Credentials', 'true');
     res.json({message:"Login successfull", token:token});
   } else {
     res.status(401);
